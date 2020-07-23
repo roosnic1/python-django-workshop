b7e2d889fc9712706c7222cc93028d9d84c621f1
Nicolas Roos <nicolas.k.roos@gmail.com>
2020-07-23 12:14:29 +0200

    added some stuff

--- example.py
+++ example.py
@@ -10,15 +10,20 @@ def hello_world_function():
 	print('Hello World')
 
 def hello_variable(name = 'Zürich'):
+	print('Hello {}'.format(name))
 	"""
 	This is a multiline
 	comment :)
 	"""
-	print('Hello {}'.format(name))
+	print('Hello %s' % name)
 
 def print_animals(animals):
 	for index, animal in enumerate(animals, start = 1):
 		print('The animal at position {} is a {}'.format(index, animal))
+
+def print_range(start = 0, stop = 10, step = 2):
+	for i in range(start, stop, step):
+		print(i)
 
 
 def print_hello():
@@ -34,6 +39,18 @@ class Human:
 		self.is_female = is_female
 
 	def greet(self):
+		if self.is_female is True:
+			print('👩: Hello everybody')
+		else:
+			print('👨: Hello everybody')
+
+	def greet2(self):
+		if self.is_female == True:
+			print('👩: Hello everybody')
+		else:
+			print('👨: Hello everybody')
+
+	def greet3(self):
 		if self.is_female:
 			print('👩: Hello everybody')
 		else:
@@ -76,6 +93,15 @@ def why_tuples():
 	movies = [('Swordfish', 'Dominic Sena', 2001), ('Snowden', ' Oliver Stone', 2016), ('Taxi Driver', 'Martin Scorsese', 1976)]
 
 
+def divide(divideBy):
+	try:
+		return 42 / divideBy
+	except ZeroDivisionError as e:
+		print('Error: Invalid argument: {}'.format(e))
+	finally:
+		print("-- division finished --")
+
+
 
 
 def deaths_in_zh_per_quartier(year1, year2, year3):
@@ -107,11 +133,16 @@ def deaths_in_zh_per_quartier(year1, year2, year3):
 
 if __name__ == '__main__':
     print_hello()
+    #print_range(0, 100, 10)
     #print_animals(ALL_THE_ANIMALS)
     #print_humans()
     #print_tuples()
     #edit_tuples()
     #why_tuples()
+    #divide(7)
+    #print('Is 🐈 in ALL_THE_ANIMALS? {}'.format('🐈' in ALL_THE_ANIMALS))
+    #print('Is 🐄 in ALL_THE_ANIMALS? {}'.format('🐄' in ALL_THE_ANIMALS))
+
     #deaths_in_zh_per_quartier(1993, 2000, 2019)
     #cowsay.dragon("Hello")
     
